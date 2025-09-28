5a47d2f1381e834ae49ac98a5e918f9464178e1c
PW - Thanh Xuan <xuanthanhpham8119@gmail.com>
2025-09-29 02:19:13 +0700

    add code lession 10

--- tests/01-pom/PersionalNotes.ts
+++ tests/01-pom/PersionalNotes.ts
@@ -1,4 +1,4 @@
-import {Page, test} from '@playwright/test';
+import {expect, Page, test} from '@playwright/test';
 import { MaterialBasePage } from './MaterialBasePage';
 
 export class PersionalNotes extends MaterialBasePage {
@@ -12,15 +12,31 @@ export class PersionalNotes extends MaterialBasePage {
     }
 
     async AddPersionalNotes() {
-        await this.page.goto("https://vnexpress.net/khoa-hoc-cong-nghe", { waitUntil: 'domcontentloaded', timeout: 60000 });
-        const title = await this.page.locator("(//h3[@class='title-news']/a)[position()<=10]").allTextContents();
-        const des = await this.page.locator("(//p[@class='description']/a)[position()<=10]").allTextContents();
-
+        await this.page.goto("https://vnexpress.net/khoa-hoc-cong-nghe");
+        const title = await this.page.locator("//h3[@class='title-news']/a").allTextContents();
+        const des = await this.page.locator("//p[@class='description']/a").allTextContents();
         await this.navigatetoPersionalNotes();
         for (let i = 0; i<10; i++) {
             await this.page.locator("//input[@id='note-title']").fill(title[i]);
             await this.page.locator("//textarea[@id='note-content']").fill(des[i]);
             await this.page.click("//button[@id='add-note']");
+        }
+    }
+
+    async SearchNote(textSearch: string) {
+        await this.page.locator("//input[@id='search']").fill(textSearch);
+    }
+
+    VerifyData(textSearch: string) {
+        return this.page.locator(`//strong[contains(normalize-space(text()),'${textSearch}')] |  //p[contains(normalize-space(text()),'${textSearch}')]`);
+    }
+
+    async VerifyDataContainsKeyWord(textSearch: string) {
+        const dataContainsKeyWord  = this.VerifyData(textSearch);
+        const soLuong = await dataContainsKeyWord.count();
+        for(let i = 0; i< soLuong; i++) {
+            const text = await dataContainsKeyWord.nth(i).textContent();
+            await expect(text?.toLowerCase()).toContain(textSearch.toLowerCase());
         }
     }
 }
--- tests/lession-10/test4.spec.ts
+++ tests/lession-10/test4.spec.ts
@@ -1,21 +1,19 @@
-import {test} from '@playwright/test';
+import {expect, test} from '@playwright/test';
 import { PersionalNotes } from '../01-pom/PersionalNotes';
+const keyWord = 'công nghệ';
 
 test ('PersionalNote', async({page}) => {
     const persionalNote = new PersionalNotes(page);
 
     await test.step('Add persional notes', async() => {
-        
         await persionalNote.AddPersionalNotes();
-        // await page.goto("https://vnexpress.net/khoa-hoc-cong-nghe");
-        // const title = await page.locator("(//h3[@class='title-news']/a)[position()<=10]").allTextContents();
-        // const des = await page.locator("(//p[@class='description']/a)[position()<=10]").allTextContents();
+    })
 
-        // await persionalNote.navigatetoPersionalNotes();
-        // for (let i = 1; i<=10; i++) {
-        //     await page.locator("//input[@id='note-title']").fill(title[i]);
-        //     await page.locator("//textarea[@id='note-content']").fill(des[i]);
-        //     await page.click("//button[@id='add-note']");
-        // }
+    await test.step('Search', async() => {
+        await persionalNote.SearchNote(keyWord);
+    })
+
+    await test.step('Verify data hiển thị đúng keyword', async() => {
+        await persionalNote.VerifyDataContainsKeyWord(keyWord);
     })
 })
